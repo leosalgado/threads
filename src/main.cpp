516e4bb5ca8c283dba5752db0b44a7c900ad3872
leosalgado <leonardosalgado@protonmail.com>
2025-05-20 15:50:41 -0300

    Add OpenMP to CSV read

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <omp.h>
 #include <string>
 
 #include <shaper.hpp>
@@ -7,7 +8,11 @@ int main() {
   const std::string INPUT_FILE = "../data/input/dataset.csv";
   const std::string OUTPUT_PATH = "../data/output/";
 
+  double start = omp_get_wtime();
   auto data = csv::read_csv(INPUT_FILE);
+
+  double end = omp_get_wtime();
+  std::cout << "Reading time: " << (end - start) << "s" << std::endl;
 
   return 0;
 }
--- src/shaper.cpp
+++ src/shaper.cpp
@@ -1,46 +1,45 @@
-#include <cstdio>
 #include <fstream>
 #include <iostream>
-#include <mutex>
 #include <omp.h>
 #include <sstream>
 #include <string>
-#include <unordered_map>
-#include <utility>
 #include <vector>
 
 #include <shaper.hpp>
-
-std::mutex file_mutex;
 
 namespace csv {
 
 CsvData read_csv(const std::string &filepath) {
   std::ifstream file(filepath);
   CsvData data;
-  std::string line;
 
   if (!file.is_open()) {
     std::cerr << "Couldn't open file: " << filepath << std::endl;
     return data;
   }
 
-  int inc = 1;
-  // while (std::getline(file, line)) {
-  for (int i = 0; i < 10; i++) {
-    std::getline(file, line);
-    std::cerr << "Linha lida: " << inc << " " << line << std::endl;
-    std::stringstream ss(line);
+  std::vector<std::string> lines;
+  std::string line;
+  while (std::getline(file, line)) {
+    lines.push_back(line);
+  }
+
+  size_t total_lines = lines.size();
+  data.rows.resize(total_lines);
+
+#pragma omp parallel for
+  for (size_t i = 0; i < total_lines; ++i) {
+    std::stringstream ss(lines[i]);
     std::string cell;
     std::vector<std::string> row;
 
     while (std::getline(ss, cell, ',')) {
       row.push_back(cell);
     }
-    inc++;
 
-    data.rows.push_back(row);
+    data.rows[i] = row;
   }
+
   return data;
 }
 
