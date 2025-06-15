58339af50dd69785fb1a5363d9284a1ba9bde50d
leosalgado <leonardosalgado@protonmail.com>
2025-06-15 17:41:04 -0300

    Modify measurements

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
+#include <chrono>
 #include <iostream>
-#include <omp.h>
 #include <string>
 
 #include <shaper.hpp>
@@ -8,17 +8,12 @@ int main() {
   const std::string INPUT_FILE = "../data/input/dataset.csv";
   const std::string OUTPUT_PATH = "../data/output/";
 
-  double start = omp_get_wtime();
+  auto start = std::chrono::high_resolution_clock::now();
   auto data = csv::read_csv(INPUT_FILE);
+  auto end = std::chrono::high_resolution_clock::now();
 
-  double end = omp_get_wtime();
-  std::cout << "Reading time: " << (end - start) << "s" << std::endl;
-
-  start = omp_get_wtime();
-  save_unique_values_per_column(data, OUTPUT_PATH);
-  end = omp_get_wtime();
-  std::cout << "Saving unique columns time: " << (end - start) << "s"
-            << std::endl;
+  std::chrono::duration<double> duration = end - start;
+  std::cout << "Reading time: " << duration.count() << "s" << std::endl;
 
   return 0;
 }
