db07242c85da67231c885617a70c1c390fb64576
leosalgado <leonardosalgado@protonmail.com>
2025-11-23 01:25:53 -0300

    Reset

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,8 @@
-#include <chrono>
 #include <iostream>
-#include <string>
-
-#include <shaper.hpp>
 
 int main() {
-  const std::string INPUT_FILE = "../data/input/dataset.csv";
-  const std::string OUTPUT_PATH = "../data/output/";
 
-  auto start = std::chrono::high_resolution_clock::now();
-  auto data = csv::read_csv(INPUT_FILE);
-  auto end = std::chrono::high_resolution_clock::now();
-
-  std::chrono::duration<double> duration = end - start;
-  std::cout << "Reading time: " << duration.count() << "s" << std::endl;
+  std::cout << "Reset World!" << std::endl;
 
   return 0;
 }
